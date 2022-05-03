226a287949ce5a968d6abc13f1fb78d28d30bd3f
zjj-comeon <1753524606@qq.com>
2022-05-03 10:01:54 +0800

    Initial commit

---
+++ src/test/java/com/database/paperms/PaperMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.paperms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
