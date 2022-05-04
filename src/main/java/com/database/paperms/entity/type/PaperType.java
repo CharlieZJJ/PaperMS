389a7a8056837faf69abd7adb941737ccf71ae29
zjj-comeon <1753524606@qq.com>
2022-05-04 23:04:28 +0800

    添加Druid、MySQL、Log4J和部分实体类

---
+++ src/main/java/com/database/paperms/entity/type/PaperType.java
@@ -0,0 +1,14 @@
+package com.database.paperms.entity.type;
+
+/**
+ * ClassName: com.database.paperms.entity.type.Paper
+ * Created by zjj
+ * Date: 2022-05-04 22:55
+ */
+public enum PaperType {
+    THEORY_PROOF,
+    REVIEW,
+    EXPERIMENTAL,
+    TOOL,
+    DATA_SET
+}
---
+++ src/main/java/com/database/paperms/entity/type/Permission.java
@@ -0,0 +1,11 @@
+package com.database.paperms.entity.type;
+
+/**
+ * ClassName: com.database.paperms.entity.type.Permission
+ * Created by zjj
+ * Date: 2022-05-04 22:47
+ */
+public enum Permission {
+    ADMIN,
+    USER
+}
