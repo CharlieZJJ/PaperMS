ec93a44aff24a53676355880b789568978f1f758
2540590989 <2540590989@qq.com>
2022-05-08 09:24:18 +0800

    this is a new update

---
+++ src/main/java/com/database/paperms/Test.java
@@ -0,0 +1,15 @@
+package com.database.paperms;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello";
+    }
+}
