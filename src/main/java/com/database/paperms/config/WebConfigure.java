c389edbfa9607c42d781ea3c326ea976d2d7d7b6
510 <2239369890@qq.com>
2022-06-12 20:21:56 +0800

    paper提交

--- src/main/java/com/database/paperms/config/WebConfigure.java
+++ src/main/java/com/database/paperms/config/WebConfigure.java
@@ -1,16 +1,16 @@
-//package com.database.paperms.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-////@Configuration
-//public class WebConfigure extends WebMvcConfigurerAdapter {
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new AuthInterceptor())
-//                .addPathPatterns("/**")
-//                .excludePathPatterns("/user/login", "/user/hello", "/admin/**", "/user/register", "/user/verify/**");
-//    }
-//}
+package com.database.paperms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+//@Configuration
+public class WebConfigure extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login", "/user/hello", "/admin/**", "/user/register", "/user/verify/**");
+    }
+}
